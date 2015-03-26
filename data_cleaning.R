1be36d058a87020363021d86ed0c0e19b16f5195
PeterWangDi <ptrwangdi@gmail.com>
2015-03-26 23:45:50 +0800

    Revert "factor as strings"
    This reverts commit 137fe333688961229febedd30a3f13ea5ce23399.

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,7 +1,1 @@
-item_dat = read.csv("tianchi_mobile_recommend_train_item.csv",header=TRUE,stringsAsFactors=FALSE)
-names(item_dat)
-user_dat = read.csv("tianchi_mobile_recommend_train_user.csv",header=TRUE,stringsAsFactors=FALSE)
-names(user_dat)
-
-sum(is.na(user_dat$user_geohash))
-class(user_dat$user_geohash)
+item_dat = read.csv("tianchi_mobile_recommend_train_item.csv",header=TRUE)
