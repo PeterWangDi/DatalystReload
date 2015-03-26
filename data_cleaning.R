137fe333688961229febedd30a3f13ea5ce23399
Steven <qianzhaozhi@yahoo,com>
2015-03-26 23:02:50 +0800

    factor as strings

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,1 +1,7 @@
-item_dat = read.csv("tianchi_mobile_recommend_train_item.csv",header=TRUE)
+item_dat = read.csv("tianchi_mobile_recommend_train_item.csv",header=TRUE,stringsAsFactors=FALSE)
+names(item_dat)
+user_dat = read.csv("tianchi_mobile_recommend_train_user.csv",header=TRUE,stringsAsFactors=FALSE)
+names(user_dat)
+
+sum(is.na(user_dat$user_geohash))
+class(user_dat$user_geohash)
